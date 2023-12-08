93b7c80920c67ac0a0fff1ade36a8c401fae0355
TranTran2002 <112295253+TranTran2002@users.noreply.github.com>
2023-12-08 15:37:23 -0800

    Add files via upload

---
+++ slot-machine.py
@@ -0,0 +1,26 @@
+#slot-machine.py
+import random
+from symtable import Symbol
+
+MAX_LINE = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+Symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+    
+}
+
+Symbol_value = {
+    "A": 3,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
